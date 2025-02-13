1bb0dd1a0cd91bae73671868b5f4fffadaa31390
Danat Nurmambetov <danat-1998@mail.ru>
2025-02-13 11:20:25 +0300

    Build brain-even and add demo.cast

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DanatN5/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DanatN5/python-project-49/actions)
+
+This is how you win:
+https://asciinema.org/a/rNIAOlFODNhZM58zPuHhLxC7q
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,34 +1,10 @@
-import random
-import prompt
-from brain_games.greeting import welcome_user, name
+from brain_games.greeting import welcome_user
+from brain_games.is_even import is_even
 
 def main():
     
-    welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    correct_answers = 0
-    while correct_answers < 3:
-        number = random.randint(0, 100)
-        print(f'Question: {number}')
-        answer = prompt.string('Your answer: ')
-        if number % 2 == 0:
-            if answer =='yes':
-                print('Correct!')
-                correct_answers += 1
-            else:
-                print(f'"{answer}" is wrong answer ;(. Correct answer was "no".')
-                break
-        else:
-            if answer == 'no':
-                print('Correct!')
-                correct_answers += 1
-            else:
-                print(f'"{answer}" is wrong answer ;(. Correct answer was "no".')
-                break
-    
-    if correct_answers == 3:
-        print(f'Congratulations, {name}!')
-    else:
-        print(f"Let's try again, {name}!")
+    name = welcome_user()
+    is_even(name)
 
-main()
+if __name__ == "__main__":
+    main()
